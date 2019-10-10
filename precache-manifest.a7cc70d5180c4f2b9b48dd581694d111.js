325be4e84502882eda033f07be1039e7d0d5ec05
Alexander Repeta <alexander.repeta@gmail.com>
2019-10-10 19:31:09 +0300

    Updates

---
+++ precache-manifest.a7cc70d5180c4f2b9b48dd581694d111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb1aee05c24488bcc8f987c1704c3f7",
+    "url": "/react-group-7/index.html"
+  },
+  {
+    "revision": "cc312a5eddba90888f9d",
+    "url": "/react-group-7/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "d9c5128b727d8ca04cdc",
+    "url": "/react-group-7/static/js/2.6fe89e9a.chunk.js"
+  },
+  {
+    "revision": "cc312a5eddba90888f9d",
+    "url": "/react-group-7/static/js/main.24b0864e.chunk.js"
+  },
+  {
+    "revision": "0e0afbbdc6ca0cc11600",
+    "url": "/react-group-7/static/js/runtime-main.33dcd3f6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-group-7/static/media/logo.25bf045c.svg"
+  }
+]);
